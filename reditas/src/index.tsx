2e86e50a56a4c20bb0e67a587d28fea6a630b19a
hstrada <6079947+hstrada@users.noreply.github.com>
2023-03-13 21:12:15 -0300

    configuring webpack

---
+++ reditas/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import { render } from 'react-dom';
+
+const root = document.getElementById('root');
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
